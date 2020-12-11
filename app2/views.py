500b530d82b48432479c981c0ac0e2a26d49de8f
sdhanush13 <56184710+sdhanush13@users.noreply.github.com>
2020-12-11 08:35:37 +0530

    Some more changes

--- app2/views.py
+++ app2/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+import datetime
 # Create your views here.
 def viewData(request):
     if request.POST:
         name=request.POST.get('name')
         dob=request.POST.get('dob')
-        print(dob)
+        insurance_type=request.POST.get('insurance_type')
+        insurnace_amt=request.POST.get('insurance_amt')
+        dob_list=dob.split()
+        print(dob_list)
     return render(request,'index.html')
